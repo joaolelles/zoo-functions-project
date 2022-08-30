e9cd5eac81a9098d13bdd68b16f19646e4c45f31
João <joaolelles7@gmail.com>
2022-08-29 23:03:18 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  
-}
-
+const getSpeciesByIds = (...ids) => {
+  const speciesByIds = [];
+  ids.forEach((id) => speciesByIds.push(species.find((specie) => specie.id === id)));
+  return speciesByIds;
+};
 module.exports = getSpeciesByIds;
