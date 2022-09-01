c40416ae35e56e57d89ccf7d06760d00ce0498f0
João <joaolelles7@gmail.com>
2022-08-31 21:51:56 -0300

    Requisito 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -16,4 +16,16 @@ describe('Testes da função HandlerElephants', () => {
   it('Testa a popularidade dos elefantes', () => {
     expect(handlerElephants('popularity')).toBe(5);
   });
+  it('Testa a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Testa a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).not.toContain('Monday');
+  });
+  it('Testa se passado um objeto vazio', () => {
+    expect(handlerElephants({})).toContain('Parâmetro inválido, é necessário uma string');
+  });
+  it('Testa se passado uma string que não contempla uma funcionalidade', () => {
+    expect(handlerElephants('')).toBeNull();
+  });
 });
