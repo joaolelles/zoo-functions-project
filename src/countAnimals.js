7e394207be6efb7c5bf7f0d9d38b5d3fb48fa8f9
João <joaolelles7@gmail.com>
2022-09-01 15:25:57 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,12 +1,26 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const handleUnder = () => {
+  const animals = {};
+  species.forEach((specie) => {
+    animals[specie.name] = specie.residents.length;
+  });
+  return animals;
+};
+
 function countAnimals(animal) {
-  if (animal === undefined) {
-    const animals = {};
-    const getAnimals = species.forEach((specie) => animals.push(specie.residents.names));
-    console.log(getAnimals);
+  if (!animal) {
+    return handleUnder();
   }
+  if (!animal.sex) {
+    const animalBuscado = species.find((obj) => obj.name === animal.specie);
+    return animalBuscado.residents.length;
+  }
+  const animalSpecie = species.find((obj) => obj.name === animal.specie);
+  const animalSex = animalSpecie.residents.filter((obj) => obj.sex === animal.sex);
+  return animalSex.length;
 }
+countAnimals({ specie: 'elephants', sex: 'male' });
 
 module.exports = countAnimals;
