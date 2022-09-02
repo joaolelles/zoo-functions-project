be72dc858f25cb87600a11d4ef9364421b9eb26f
João <joaolelles7@gmail.com>
2022-09-01 21:29:19 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  return { child: child.length, adult: adult.length, senior: senior.length };
+  // console.log({ child: child.length, adult: adult.length, senior: senior.length });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const callFun = countEntrants(entrants);
+  const { child, adult, senior } = callFun;
+  const { prices } = data;
+  return (child * prices.child + adult * prices.adult + senior * prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
